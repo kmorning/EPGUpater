f705988d46aed037473f6a0046dbfb3cad58f102
kmorning <kleemorning@gmail.com>
2018-02-10 22:52:12 -0500

    Add service launcher

--- app/src/main/java/org/duckdns/altered/epgupdater/MainActivity.java
+++ app/src/main/java/org/duckdns/altered/epgupdater/MainActivity.java
@@ -1,13 +1,23 @@
 package org.duckdns.altered.epgupdater;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
+import org.duckdns.altered.backgroundservice.EPGUpdateService;
+
 public class MainActivity extends Activity {
+    private Intent mServiceIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void launchEPGUpdateService() {
+        mServiceIntent = new Intent(this, EPGUpdateService.class);
+        startService(mServiceIntent);
     }
 }
