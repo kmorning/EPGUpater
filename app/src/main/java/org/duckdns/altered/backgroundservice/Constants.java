8f7c03bda4d6c5d8359f5b31d9fa465a30ef8356
kmorning <kleemorning@gmail.com>
2018-02-10 22:57:31 -0500

    Initial commit

---
+++ app/src/main/java/org/duckdns/altered/backgroundservice/Constants.java
@@ -0,0 +1,12 @@
+package org.duckdns.altered.backgroundservice;
+
+/**
+ * Created by kmorning on 2018-02-10.
+ *
+ * Constants used by multiple classes in this package
+ */
+
+public class Constants {
+    // Defines a custom Intent action
+    public static final String BROADCAST_ACTION = "org.duckdns.altered.epgupdater.BROADCAST";
+}
