6ba947d232e2b51f8d1f64ea4725ba8aa47974a7
kmorning <kleemorning@gmail.com>
2018-02-11 01:29:58 -0500

    implement onHandleIntent

--- app/src/main/java/org/duckdns/altered/epgupdater/EPGUpdateService.java
+++ app/src/main/java/org/duckdns/altered/epgupdater/EPGUpdateService.java
@@ -3,6 +3,14 @@ package org.duckdns.altered.epgupdater;
 import android.app.IntentService;
 import android.content.Intent;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 /**
  * Created by kmorning on 2018-02-10.
  *
@@ -20,7 +28,17 @@ public class EPGUpdateService extends IntentService {
     // Used to write to the system log from this class.
     public static final String LOG_TAG = "EPGUpdateService";
 
+    // Defines and instantiates an object for handling status updates.
+    private BroadcastNotifier mBroadcaster = new BroadcastNotifier(this);
 
+    // HTTP InputStream read buffer size
+    private static final int BUFFER_SIZE = 4096;
+
+    /**
+     * An IntentService must always have a constructor that calls the super constructor. The
+     * string supplied to the super constructor is used to give a name to the IntentService's
+     * background thread.
+     */
     public EPGUpdateService() {
         // Worker thread name which is only important for debugging
         super("EPGUpdateService");
@@ -34,6 +52,72 @@ public class EPGUpdateService extends IntentService {
      */
     @Override
     protected void onHandleIntent(Intent workIntent) {
+        // Gets a URL to read from the incoming Intent's "data" value
+        String urlString = workIntent.getDataString();
 
+        // URL to download from
+        URL url;
+
+        //block that tries to connect to xml data, and throws an IOException if one occurs
+        try {
+            // Convert the incoming data string to a URL.
+            url = new URL(urlString);
+
+           /**
+            * Tries to open a connection to the URL. If an IO error occurs, this throws an
+            * IOException
+            */
+            URLConnection urlConnection = url.openConnection();
+
+            // If the connection is an HTTP connection, continue
+            if (urlConnection instanceof HttpURLConnection) {
+                // Broadcasts an Intent indicating that processing has started.
+                mBroadcaster.broadcastIntentWithState(Constants.STATE_ACTION_STARTED);
+
+                // Casts the connection to a HTTP connection
+                HttpURLConnection httpURLConnection = (HttpURLConnection) urlConnection;
+
+                // Reports that the service is about to connect to the RSS feed
+                mBroadcaster.broadcastIntentWithState(Constants.STATE_ACTION_CONNECTING);
+
+                // Get response code from site
+                int responseCode = httpURLConnection.getResponseCode();
+
+                // Continue if response is OK
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    // Get content length (in bytes) to calculate progress from
+                    int contentLength = httpURLConnection.getContentLength();
+
+                    InputStream inputStream = urlConnection.getInputStream();
+
+                    // Save to file
+                    FileOutputStream outputStream = new FileOutputStream(Constants.XML_SAVE_FILE_PATH);
+
+                    // keep track of how much is downloaded
+                    int totalBytesRead = 0;
+                    int progressDone = 0;
+                    // TODO: broadcast status downloading
+
+                    int bytesRead = -1;
+                    byte[] buffer = new byte[BUFFER_SIZE];
+                    while ((bytesRead = inputStream.read(buffer)) != -1) {
+                        outputStream.write(buffer, 0, bytesRead);
+                        totalBytesRead += bytesRead;
+                        progressDone = totalBytesRead/contentLength * 100;
+                        // TODO: broadcast progress
+                    }
+
+                    outputStream.close();
+                    inputStream.close();
+                }
+
+            }
+        }
+        catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
