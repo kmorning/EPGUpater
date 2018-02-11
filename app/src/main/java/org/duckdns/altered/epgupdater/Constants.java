6286398dfe1c42bc0b9ac19ecd7b8b619ac00c4e
kmorning <kleemorning@gmail.com>
2018-02-11 00:47:11 -0500

    Added broadcast statuses

--- app/src/main/java/org/duckdns/altered/epgupdater/Constants.java
+++ app/src/main/java/org/duckdns/altered/epgupdater/Constants.java
@@ -15,4 +15,25 @@ public class Constants {
 
     // Defines the key for the log "extra" in an Intent
     public static final String EXTENDED_STATUS_LOG = "org.duckdns.altered.epgupdater.LOG";
+
+    // Status values to broadcast
+
+    // The download is starting
+    public static final int STATE_ACTION_STARTED = 0;
+
+    // The background thread is connecting to the RSS feed
+    public static final int STATE_ACTION_CONNECTING = 1;
+
+    // The background thread is parsing the RSS feed
+    public static final int STATE_ACTION_PARSING = 2;
+
+    // The background thread is writing data to the content provider
+    public static final int STATE_ACTION_WRITING = 3;
+
+    // The background thread is done
+    public static final int STATE_ACTION_COMPLETE = 4;
+
+    // The background thread is doing logging
+    public static final int STATE_LOG = -1;
+
 }
