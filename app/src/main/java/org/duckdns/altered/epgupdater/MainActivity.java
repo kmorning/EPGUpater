c83a1e388eac5c58da3a645e805c5b083e884344
kmorning <kleemorning@gmail.com>
2018-02-11 12:16:14 -0500

    Add broadcast receiver and on receive action

--- app/src/main/java/org/duckdns/altered/epgupdater/MainActivity.java
+++ app/src/main/java/org/duckdns/altered/epgupdater/MainActivity.java
@@ -1,8 +1,13 @@
 package org.duckdns.altered.epgupdater;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
     private Intent mServiceIntent;
@@ -14,8 +19,47 @@ public class MainActivity extends Activity {
 
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Register for the broadcast based on ACTION string
+        IntentFilter filter = new IntentFilter(Constants.BROADCAST_ACTION);
+        LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiver, filter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Unregister broadcast receiver when application is paused
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(broadcastReceiver);
+    }
+
     public void launchEPGUpdateService() {
         mServiceIntent = new Intent(this, EPGUpdateService.class);
         startService(mServiceIntent);
     }
+
+    // Define the callback for broadcast data received
+    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int statusCode = intent.getIntExtra(Constants.EXTENDED_DATA_STATUS,
+                    Constants.STATE_ACTION_FAILED);
+            TextView textView = findViewById(R.id.textView);
+
+            switch (statusCode) {
+                case Constants.STATE_ACTION_STARTED:
+                    textView.setText(getString(R.string.status_start));
+                case Constants.STATE_ACTION_CONNECTING:
+                    textView.setText(getString(R.string.status_connect));
+                case Constants.STATE_ACTION_DOWNLOADING:
+                    textView.setText(getString(R.string.status_download));
+                case Constants.STATE_ACTION_DOWNLOAD_COMPLETE:
+                    textView.setText(getString(R.string.status_download_done));
+                case Constants.STATE_ACTION_FAILED:
+                    textView.setText(getString(R.string.status_fail));
+                default:
+            }
+        }
+    };
 }
